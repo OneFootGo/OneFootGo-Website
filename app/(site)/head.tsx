d27f1f298f620f268bd4de1549d743e52b27be44
arnavchauhan <arnav216@gmail.com>
2024-07-10 09:57:15 +0530

    more changes to landing page

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title> OneFootGo | Next.js Template for Startup and SaaS</title>
+      <title> OneFootGo | Pro-Grade Sports Analysis Tools</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
       <link rel="icon" href="/images/favicon.ico" />
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -79,13 +79,13 @@ const Hero = () => {
                 <div className=" relative aspect-[700/444] w-full">
                   <Image
                     className="shadow-solid-l dark:hidden"
-                    src="/images/hero/hero-light.svg"
+                    src="/images/hero/pawelzmarlak-2024-07-09T07_30_36.992Z.png"
                     alt="Hero"
                     fill
                   />
                   <Image
                     className="hidden shadow-solid-l dark:block"
-                    src="/images/hero/hero-dark.svg"
+                    src="/images/hero/hero-dark.png"
                     alt="Hero"
                     fill
                   />

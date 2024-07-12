7084aa0709af3498e4bc9b1cca5b8018d730721d
arnavchauhan <arnav216@gmail.com>
2024-07-12 17:05:03 +0530

    responsiveness changes, add images on mobile

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -22,187 +22,177 @@ const About = () => {
           </div>
           </section>  
       {/* <!-- ===== About Start ===== --> */}
-      <section className="overflow-hidden pb-20 lg:pb-25 xl:pb-30">
-        <div className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0">
-          <div className="flex items-center gap-8 lg:gap-32.5">
-            <motion.div
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  x: -20,
-                },
-
-                visible: {
-                  opacity: 1,
-                  x: 0,
-                },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 0.5, delay: 0.1 }}
-              viewport={{ once: true }}
-              className="animate_left relative mx-auto hidden aspect-[588/526.5] md:block md:w-1/2"
-            >
-              <Image
-                src="/images/about/HPE_light.png"
-                alt="About"
-                className="dark:hidden"
-                width={588}
-                height={526.5}
-              />
-              <Image
-                src="/images/about/HPE_dark.png"
-                alt="About"
-                className="hidden dark:block"
-                width={588}
-                height={526.5}
-              />
-            </motion.div>
-            <motion.div
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  x: 20,
-                },
-
-                visible: {
-                  opacity: 1,
-                  x: 0,
-                },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 0.5, delay: 0.1 }}
-              viewport={{ once: true }}
-              className="animate_right md:w-1/2"
-            >
-              <span className="font-medium uppercase text-black dark:text-white">
-                <span className="mb-4 mr-4 inline-flex rounded-full bg-meta px-4.5 py-1 text-metatitle uppercase text-white">
-                  Coming Soon
-                </span>{" "}
-              </span>
-              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
-                Advanced Form Correction with 
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
-                  Human Pose Estimation
-                </span>
-              </h2>
-              <p>
-                With powerful machine learning models and player footage, OneFootGo can provide detailed insights about your form. Our platform can analyze your movements and empower you to improve you game.
-              </p>
-
-              <div className="mt-7.5 flex items-center gap-5">
-                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
-                  <p className="text-metatitle2 font-semibold text-black dark:text-white">
-                    01
-                  </p>
-                </div>
-                <div className="w-3/4">
-                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
-                    Highly Accurate Joint Tracking
-                  </h3>
-                  <p>Trained on custom datasets, our models are designed specifically for personalized form correction.</p>
-                </div>
-              </div>
-              <div className="mt-7.5 flex items-center gap-5">
-                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
-                  <p className="text-metatitle2 font-semibold text-black dark:text-white">
-                    02
-                  </p>
-                </div>
-                <div className="w-3/4">
-                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
-                    Comprehensive Reporting
-                  </h3>
-                  <p>Access detailed reports that highlight your performance metrics and improvement over time.</p>
-                </div>
-              </div>
-            </motion.div>
+<section className="overflow-hidden pb-20 lg:pb-25 xl:pb-30">
+  <div className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0">
+    <div className="flex flex-col md:flex-row items-center gap-8 lg:gap-32.5">
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+            x: -20,
+          },
+          visible: {
+            opacity: 1,
+            x: 0,
+          },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: 0.5, delay: 0.1 }}
+        viewport={{ once: true }}
+        className="animate_left relative mx-auto md:block md:w-1/2 w-full aspect-[588/526.5] mb-8 md:mb-0"
+      >
+        <Image
+          src="/images/about/HPE_light.png"
+          alt="About"
+          className="dark:hidden"
+          width={588}
+          height={526.5}
+        />
+        <Image
+          src="/images/about/HPE_dark.png"
+          alt="About"
+          className="hidden dark:block"
+          width={588}
+          height={526.5}
+        />
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+            x: 20,
+          },
+          visible: {
+            opacity: 1,
+            x: 0,
+          },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: 0.5, delay: 0.1 }}
+        viewport={{ once: true }}
+        className="animate_right md:w-1/2 w-full"
+      >
+        <span className="font-medium uppercase text-black dark:text-white">
+          <span className="mb-4 mr-4 inline-flex rounded-full bg-meta px-4.5 py-1 text-metatitle uppercase text-white">
+            Coming Soon
+          </span>{" "}
+        </span>
+        <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+          Advanced Form Correction with {" "}
+          <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+            Human Pose Estimation
+          </span>
+        </h2>
+        <p>
+          With powerful machine learning models and player footage, OneFootGo can provide detailed insights about your form. Our platform can analyze your movements and give you the ability to improve your game.
+        </p>
+        <div className="mt-7.5 flex items-center gap-5">
+          <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+            <p className="text-metatitle2 font-semibold text-black dark:text-white">
+              01
+            </p>
+          </div>
+          <div className="w-3/4">
+            <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+              Highly Accurate Joint Tracking
+            </h3>
+            <p>Trained on custom datasets, our models are designed specifically for personalized form correction.</p>
           </div>
         </div>
-      </section>
-      {/* <!-- ===== About End ===== --> */}
-
-      {/* <!-- ===== About Two Start ===== --> */}
-      <section>
-        <div className="mx-auto max-w-c-1235 overflow-hidden px-4 md:px-8 2xl:px-0">
-          <div className="flex items-center gap-8 lg:gap-32.5">
-            <motion.div
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  x: -20,
-                },
-
-                visible: {
-                  opacity: 1,
-                  x: 0,
-                },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 1, delay: 0.1 }}
-              viewport={{ once: true }}
-              className="animate_left md:w-1/2"
-            >
-              <h4 className="font-medium uppercase text-black dark:text-white">
-                Track your stats
-              </h4>
-              <h2 className="relative mb-6 text-4xl font-bold text-black dark:text-white xl:text-hero">
-                Track your Progress and {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
-                  Elevate Your Game
-                </span>
-              </h2>
-              <p>
-                Get stats that give you the ability to improve immediately with in-depth analysis of your gameplay. Highlights include shot speed, player speed, and accurate ball tracking to help you make data driven decisions.
-              </p>
-              <div>
-                <a
-                  href="#"
-                  className="group mt-7.5 inline-flex items-center gap-2.5 text-black hover:text-primary dark:text-white dark:hover:text-primary"
-                >
-                  
-                </a>
-              </div>
-            </motion.div>
-            <motion.div
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  x: 20,
-                },
-
-                visible: {
-                  opacity: 1,
-                  x: 0,
-                },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 1, delay: 0.1 }}
-              viewport={{ once: true }}
-              className="animate_right relative mx-auto hidden aspect-[588/526.5] md:block md:w-1/2"
-            >
-              <Image
-                src="/images/hero/hero-light.png"
-                alt="About"
-                className="dark:hidden"
-                width={588}
-                height={526.5}
-              />
-              <Image
-                src="/images/hero/hero-dark.png"
-                alt="About"
-                className="hidden dark:block"
-                width={588}
-                height={526.5}
-              />
-            </motion.div>
+        <div className="mt-7.5 flex items-center gap-5">
+          <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+            <p className="text-metatitle2 font-semibold text-black dark:text-white">
+              02
+            </p>
+          </div>
+          <div className="w-3/4">
+            <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+              Comprehensive Reporting
+            </h3>
+            <p>Access detailed reports that highlight your performance metrics and improvement over time.</p>
           </div>
         </div>
-      </section>
-      {/* <!-- ===== About Two End ===== --> */}
+      </motion.div>
+    </div>
+  </div>
+</section>
+{/* <!-- ===== About End ===== --> */}
+
+{/* <!-- ===== About Two Start ===== --> */}
+<section className="overflow-hidden pb-20 lg:pb-25 xl:pb-30">
+  <div className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0">
+    <div className="flex flex-col md:flex-row items-center gap-8 lg:gap-32.5">
+      {/* Image container */}
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+            x: -20,
+          },
+          visible: {
+            opacity: 1,
+            x: 0,
+          },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: 1, delay: 0.1 }}
+        viewport={{ once: true }}
+        className="relative w-full md:w-1/2 aspect-[588/526.5] md:order-1 mt-4 md:mb-0"
+      >
+        <Image
+          src="/images/hero/hero-light.png"
+          alt="About Two"
+          className="dark:hidden"
+          width={588}
+          height={526.5}
+        />
+        <Image
+          src="/images/hero/hero-dark.png"
+          alt="About Two"
+          className="hidden dark:block"
+          width={588}
+          height={526.5}
+        />
+      </motion.div>
+
+      {/* Text container */}
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+            x: 20,
+          },
+          visible: {
+            opacity: 1,
+            x: 0,
+          },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: 1, delay: 0.1 }}
+        viewport={{ once: true }}
+        className="relative w-full md:w-1/2 md:order-2"
+      >
+        <h4 className="font-medium uppercase text-black dark:text-white">
+          Track your stats
+        </h4>
+        <h2 className="mb-6 text-4xl font-bold text-black dark:text-white xl:text-hero">
+          Track your Progress and
+          <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+            Elevate Your Game
+          </span>
+        </h2>
+        <p>
+          Get stats that give you the ability to improve immediately with in-depth analysis of your gameplay. Highlights include shot speed, player speed, and accurate ball tracking to help you make data-driven decisions.
+        </p>
+      </motion.div>
+    </div>
+  </div>
+</section>
+{/* <!-- ===== About Two End ===== --> */}
     </>
   );
 };
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -37,86 +37,62 @@ const Hero = () => {
     <>
       <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
-          <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
-            <div className=" md:w-1/2">
-              <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🎾  OneFootGo - Sports Tools to Maximize your Performance
-              </h4>
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Professional-Grade Sports Analysis Tools for {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  Everyone
-                </span>
-              </h1>
-              <p>
-                OneFootGo - A sports analysis platform helping athletes of all levels to maximize their performance. With the power of AI and Machine Learning, get personalized insights and recommendations on everything from your first tennis serve to your last golf swing.
-              </p>
-
-              <div className="mt-10">
-                <form onSubmit={handleSubmit} >
-                  <div className="flex flex-wrap gap-5">
-                    <input
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="email"
-                      placeholder="Enter your email address"
-                      className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
-                    />
-                    <button
-                      aria-label="get started button"
-                      className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
-                    >
-                      Get Started
-                    </button>
-                  </div>
-                </form>
-
-                <p className="mt-5 text-black dark:text-white">
-                  {message}
-                </p>
-              </div>
-            </div>
-
-            <div className="animate_right hidden md:w-1/2 lg:block">
-              <div className="relative 2xl:-mr-7.5">
-                <Image
-                  src="/images/shape/shape-01.png"
-                  alt="shape"
-                  width={46}
-                  height={246}
-                  className="absolute -left-11.5 top-0"
-                />
-                <Image
-                  src="/images/shape/shape-02.svg"
-                  alt="shape"
-                  width={36.9}
-                  height={36.7}
-                  className="absolute bottom-0 right-0 z-10"
-                />
-                <Image
-                  src="/images/shape/shape-03.svg"
-                  alt="shape"
-                  width={21.64}
-                  height={21.66}
-                  className="absolute -right-6.5 bottom-0 z-1"
-                />
-                <div className=" relative aspect-[700/444] w-full">
-                  <Image
-                    className="shadow-solid-l dark:hidden"
-                    src="/images/hero/hero-light.png"
-                    alt="Hero"
-                    fill
-                  />
-                  <Image
-                    className="hidden shadow-solid-l dark:block"
-                    src="/images/hero/hero-dark.png"
-                    alt="Hero"
-                    fill
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="flex flex-col lg:flex-row lg:items-center lg:gap-8 xl:gap-32.5">
+  <div className="md:w-1/2 order-2 lg:order-none">
+    <h4 className="mt-3 mb-4.5 text-lg font-medium text-black dark:text-white">
+      🎾  OneFootGo - Sports Tools to Maximize your Performance
+    </h4>
+    <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero">
+      Professional-Grade Sports Analysis Tools for{" "}
+      <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+        Everyone
+      </span>
+    </h1>
+    <p>
+      OneFootGo - A sports analysis platform helping athletes of all levels to maximize their performance. With the power of AI and Machine Learning, get personalized insights and recommendations on everything from your first tennis serve to your last golf swing.
+    </p>
+    <div className="mt-10">
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-wrap gap-5">
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Enter your email address"
+            className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
+          />
+          <button
+            aria-label="get started button"
+            className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
+          >
+            Get Started
+          </button>
+        </div>
+      </form>
+      <p className="mt-5 text-black dark:text-white">
+        {message}
+      </p>
+    </div>
+  </div>
+  <div className="md:w-1/2 sm:w-full order-1 lg:order-none sm:block">
+    <div className="relative 2xl:-mr-7.5">
+      <div className=" relative aspect-[700/444] w-full">
+        <Image
+          className="shadow-solid-l dark:hidden"
+          src="/images/hero/hero-light.png"
+          alt="Hero"
+          fill
+        />
+        <Image
+          className="hidden shadow-solid-l dark:block"
+          src="/images/hero/hero-dark.png"
+          alt="Hero"
+          fill
+        />
+      </div>
+    </div>
+  </div>
+</div>
         </div>
       </section>
     </>
@@ -124,3 +100,5 @@ const Hero = () => {
 };
 
 export default Hero;
+
+
